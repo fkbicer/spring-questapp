5eaeeaf3dfde412cc97bf53f718dc77e7ffb85b5
furkankaan <fkbicer07@gmail.com>
2023-09-09 16:58:58 +0300

    entity id's auto increment

--- src/main/java/com/project/questapp/entities/Post.java
+++ src/main/java/com/project/questapp/entities/Post.java
@@ -9,6 +9,8 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Lob;
@@ -22,6 +24,7 @@ import lombok.Data;
 public class Post {
     
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name="user_id", nullable = false)
