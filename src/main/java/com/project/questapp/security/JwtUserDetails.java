0c48431ce839b256715bd2261c5b3ee7bc02e325
furkankaan <fkbicer07@gmail.com>
2023-09-13 12:06:37 +0300

    ALLLLLL FAKIN SECURITY SETUP AND INTEGRATION

--- src/main/java/com/project/questapp/security/JwtUserDetails.java
+++ src/main/java/com/project/questapp/security/JwtUserDetails.java
@@ -17,12 +17,12 @@ import lombok.Setter;
 @Setter
 public class JwtUserDetails implements UserDetails {
 
-    public Long id;
-    private String username;
-    private String password;
-    private Collection<? extends GrantedAuthority> authorities;
-    
-    private JwtUserDetails (Long id, String username, String password,Collection<? extends GrantedAuthority> authorities ) {
+	public Long id;
+	private String username;
+	private String password;
+	private Collection<? extends GrantedAuthority> authorities;
+	
+    private JwtUserDetails(Long id, String username, String password, Collection<? extends GrantedAuthority> authorities) {
         this.id = id;
         this.username = username;
         this.password = password;
@@ -32,28 +32,27 @@ public class JwtUserDetails implements UserDetails {
     public static JwtUserDetails create(User user) {
         List<GrantedAuthority> authoritiesList = new ArrayList<>();
         authoritiesList.add(new SimpleGrantedAuthority("user"));
-        return new JwtUserDetails(user.getId(),user.getUserName(), user.getPassword(), authoritiesList);
-    }
-
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
+        return new JwtUserDetails(user.getId(), user.getUserName(), user.getPassword(), authoritiesList);
     }
     
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+
 }
--- src/main/java/com/project/questapp/services/UserDetailsServiceImpl.java
+++ src/main/java/com/project/questapp/services/UserDetailsServiceImpl.java
@@ -3,11 +3,13 @@ package com.project.questapp.services;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import com.project.questapp.entities.User;
 import com.project.questapp.repos.UserRepository;
 import com.project.questapp.security.JwtUserDetails;
 
+@Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     private UserRepository userRepository;
